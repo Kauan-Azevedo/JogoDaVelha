040999cbfc61b4f08157193ae80a553e468b226e
Kauan Sotério de Azevedo <kauan.azevedo@jeaholding.com.br>
2023-03-29 19:32:56 -0300

    feat: server

---
+++ server.cjs
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, "dist")));
+app.use("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist/index.html"));
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(
+        `Aplication running on http://localhost:${process.env.PORT || 3000}`
+    );
+});
